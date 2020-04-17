0828475be523e27e6fc809e21ed1149972613dac
ragadapa <me.ragadapa@gmail.com>
2020-04-17 10:42:41 +0530

    initial version:wq

---
+++ src/main/java/com/example/srpingboot/SrpingBootApplication.java
@@ -0,0 +1,15 @@
+package com.example.srpingboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrpingBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrpingBootApplication.class, args);
+		
+		System.out.println("working");
+	}
+
+}
